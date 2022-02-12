4efeaa9df21d046d389c34b937b420cdfb8042c5
Colin Hoolwerf <choolwerf@gmail.com>
2022-02-11 18:50:31 -0600

    first part complete, players rendering in browser

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -5,7 +5,14 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 
 export const fetchAllPlayers = async () => {
-
+    try {
+        let response = await fetch(`${APIURL}/players`);
+        let result = await response.json();
+        if (result.error) throw result.error;
+        return result.data.players;
+    } catch (error) {
+        console.log("There was an error:", error);
+    }
 };
 
 export const fetchSinglePlayer = async (playerId) => {
