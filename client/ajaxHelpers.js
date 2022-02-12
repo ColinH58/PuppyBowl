805ec089ca27ec9c4f9cba927af8dbe49ddc1ed1
Colin Hoolwerf <choolwerf@gmail.com>
2022-02-11 19:47:14 -0600

    so far so ok I guess

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -6,17 +6,24 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 export const fetchAllPlayers = async () => {
     try {
-        let response = await fetch(`${APIURL}/players`);
-        let result = await response.json();
-        if (result.error) throw result.error;
-        return result.data.players;
+        let response = await fetch(`${APIURL}/players`)
+        let result = await response.json()
+        if (result.error) throw result.error
+        return result.data.players
     } catch (error) {
-        console.log("There was an error:", error);
+        console.log("There was an error:", error)
     }
 };
 
 export const fetchSinglePlayer = async (playerId) => {
-
+    try {
+        let response = await fetch(`${APIURL}/players/${playerId}`)
+        let result = await response.json()
+        // console.log(result.data.player)
+        return result.data.player
+    } catch (error) {
+        console.log("There was an error:", error)
+    }
 };
 
 export const addNewPlayer = async (playerObj) => {
